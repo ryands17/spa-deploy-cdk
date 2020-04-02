e2b1b06da35140b432bb50ca78e8f5d729f03aa3
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2020-04-03 01:17:22 +0530

    add cloudfront and IAM roles

--- lib/aws-cdk-stack.ts
+++ lib/aws-cdk-stack.ts
@@ -1,16 +1,59 @@
 import * as cdk from '@aws-cdk/core';
 import * as S3 from '@aws-cdk/aws-s3';
-import { BUCKET_NAME } from './config';
+import * as IAM from '@aws-cdk/aws-iam';
+import {
+  CloudFrontWebDistribution,
+  CloudFrontWebDistributionProps,
+  OriginAccessIdentity,
+} from '@aws-cdk/aws-cloudfront';
+import { BUCKET_NAME, WEBSITE_NAME } from './config';
 
 export class AwsCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // create an S3 bucket
+    // S3 bucket for a static website
     const bucket = new S3.Bucket(this, BUCKET_NAME, {
       websiteIndexDocument: 'index.html',
       websiteErrorDocument: 'index.html',
     });
-    bucket.grantPublicAccess('*', 's3:GetObject');
+    // bucket.grantPublicAccess('*', 's3:GetObject');
+
+    // cloudfront dist for cdn & https
+    const cloudFrontOAI = new OriginAccessIdentity(this, 'OAI', {
+      comment: `OAI for ${WEBSITE_NAME} website.`,
+    });
+
+    let cloudFrontDistProps: CloudFrontWebDistributionProps = {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentity: cloudFrontOAI,
+          },
+          behaviors: [{ isDefaultBehavior: true }],
+          originPath: `/`,
+        },
+      ],
+    };
+
+    new CloudFrontWebDistribution(
+      this,
+      `${WEBSITE_NAME}-cfn`,
+      cloudFrontDistProps
+    );
+
+    // add IAM roles for Cloudfront only access to S3
+    const policyStatement = new IAM.PolicyStatement();
+    policyStatement.addActions('s3:GetBucket*');
+    policyStatement.addActions('s3:GetObject*');
+    policyStatement.addActions('s3:List*');
+    policyStatement.addResources(bucket.bucketArn);
+    policyStatement.addResources(`${bucket.bucketArn}/*`);
+    policyStatement.addCanonicalUserPrincipal(
+      cloudFrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+    );
+
+    bucket.addToResourcePolicy(policyStatement);
   }
 }
--- lib/config.ts
+++ lib/config.ts
@@ -1,1 +1,2 @@
+export const WEBSITE_NAME = 'my-website';
 export const BUCKET_NAME = 'my-site-test';
